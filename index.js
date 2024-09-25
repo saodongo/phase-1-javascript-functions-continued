5b7467823a92551e92c8b6b5390838ac3fcc7368
sharonodongo <sharon94odongo@gmail.com>
2024-09-25 07:07:16 +0300

    submitting assignment

--- index.js
+++ index.js
@@ -14,16 +14,16 @@ function wrapAdjective(flair = "*") {
 
 const encouragingPromptFunction = wrapAdjective("!!!");
 console.log(encouragingPromptFunction("a dedicated programmer")); 
-// Output: "You are !!!a dedicated programmer!!!"
+"You are !!!a dedicated programmer!!!"
 
 console.log(saturdayFun()); 
-// Output: "This Saturday, I want to roller-skate!"
+"This Saturday, I want to roller-skate!"
 
 console.log(saturdayFun("bathe the dog")); 
-// Output: "This Saturday, I want to bathe the dog!"
+"This Saturday, I want to bathe the dog!"
 
 console.log(mondayWork()); 
-// Output: "This Monday, I will go to the office."
+"This Monday, I will go to the office."
 
 console.log(mondayWork("work from home")); 
-// Output: "This Monday, I will work from home."
+"This Monday, I will work from home."
