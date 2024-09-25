97da254bee638d1dca449f096fb3b35396c23040
sharonodongo <sharon94odongo@gmail.com>
2024-09-25 05:59:33 +0300

    submitting assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(task = "go to the office") {
+    return `This Monday, I will ${task}.`;
+}
+
+function wrapAdjective(flair = "*") {
+    return function (adjective = "special") {
+        return `You are ${flair}${adjective}${flair}!`;
+    };
+}
+
+const encouragingPromptFunction = wrapAdjective("!!!");
+console.log(encouragingPromptFunction("a dedicated programmer")); 
+// Output: "You are !!!a dedicated programmer!!!"
+
+console.log(saturdayFun()); 
+// Output: "This Saturday, I want to roller-skate!"
+
+console.log(saturdayFun("bathe the dog")); 
+// Output: "This Saturday, I want to bathe the dog!"
+
+console.log(mondayWork()); 
+// Output: "This Monday, I will go to the office."
+
+console.log(mondayWork("work from home")); 
+// Output: "This Monday, I will work from home."
